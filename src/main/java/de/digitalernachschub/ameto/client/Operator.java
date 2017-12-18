e57d50861df52a32bf3bd7e6f08fe61e1a609ce7
Michael Seifert <m.seifert@digitalernachschub.de>
2017-12-18 07:58:00 +0100

    Added javadoc comments.

--- src/main/java/de/digitalernachschub/ameto/client/Operator.java
+++ src/main/java/de/digitalernachschub/ameto/client/Operator.java
@@ -2,6 +2,9 @@ package de.digitalernachschub.ameto.client;
 
 import lombok.Value;
 
+/**
+ * Represents a processing step in a pipeline.
+ */
 @Value
 public class Operator {
     String name;
--- src/main/java/de/digitalernachschub/ameto/client/Pipeline.java
+++ src/main/java/de/digitalernachschub/ameto/client/Pipeline.java
@@ -4,6 +4,10 @@ import lombok.Value;
 
 import java.util.List;
 
+/**
+ * Represents a processing pipeline.
+ * A pipeline consists of a unique name and an ordered list of processing steps, the operators.
+ */
 @Value
 public class Pipeline {
     String name;
