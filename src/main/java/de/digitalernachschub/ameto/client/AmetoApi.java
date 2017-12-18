dd5a64798e6dbbf9911b8c9de4fd21d0a8341427
Michael Seifert <m.seifert@digitalernachschub.de>
2017-12-18 11:10:20 +0100

    Changed visibility of AmetoApi to default visibilty.

--- src/main/java/de/digitalernachschub/ameto/client/AmetoApi.java
+++ src/main/java/de/digitalernachschub/ameto/client/AmetoApi.java
@@ -7,7 +7,7 @@ import retrofit2.http.POST;
 
 import java.util.List;
 
-public interface AmetoApi {
+interface AmetoApi {
     @POST("/pipeline")
     Call<Void> add(@Body Pipeline pipeline);
     @GET("/pipeline")
