bab233c8b14abe726e905bf5d5f7ca160d526995
Michael Seifert <m.seifert@digitalernachschub.de>
2017-12-21 09:00:30 +0100

    CI script tears down the test env.

--- ci.sh
+++ ci.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
+setup_test_env() {
+    docker-compose -f docker-compose.ci.yml up -d kafka
+    sleep 10
+    docker-compose -f docker-compose.ci.yml up -d
+}
+
+run_tests() {
+    docker run --network=javaclient_default --env AMETO_API_URL=http://api:5000 --rm ameto/java-client-tests
+}
+
+tear_down_test_env() {
+    docker-compose -f docker-compose.ci.yml down
+}
+
 docker build -f Dockerfile.test -t ameto/java-client-tests .
 
-docker-compose -f docker-compose.ci.yml up -d kafka
-sleep 10
-docker-compose -f docker-compose.ci.yml up -d
-
-docker run --network=javaclient_default --env AMETO_API_URL=http://api:5000 --rm ameto/java-client-tests
+trap tear_down_test_env EXIT
+setup_test_env
+run_tests
