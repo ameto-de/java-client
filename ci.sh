2ffe19fad1a1a5f7ef36f7dcf5322109c6dcc559
Michael Seifert <m.seifert@digitalernachschub.de>
2017-12-21 08:57:29 +0100

    Added a docker-compose configuration for CI which does not publish ports. This reduces the chance of a port collision during the CI build.

--- ci.sh
+++ ci.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 docker build -f Dockerfile.test -t ameto/java-client-tests .
 
-docker-compose up -d kafka
+docker-compose -f docker-compose.ci.yml up -d kafka
 sleep 10
-docker-compose up -d
+docker-compose -f docker-compose.ci.yml up -d
 
 docker run --network=javaclient_default --env AMETO_API_URL=http://api:5000 --rm ameto/java-client-tests
