cdbc35fbd334c2a200f7297ef8edd06eb304d2f3
Michael Seifert <m.seifert@digitalernachschub.de>
2018-01-31 14:49:26 +0100

    Added test case which asserts that ameto can process a JPEG image and the processed image can be retrieved.

--- src/test/java/de/digitalernachschub/ameto/client/AmetoTest.java
+++ src/test/java/de/digitalernachschub/ameto/client/AmetoTest.java
@@ -1,9 +1,14 @@
 package de.digitalernachschub.ameto.client;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collections;
@@ -11,15 +16,16 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class AmetoTest {
     private static Ameto ameto;
+    private static String deliveryBaseUrl;
 
     @Before
     public void setUp() {
         String apiUrl = System.getenv().getOrDefault("AMETO_API_URL", "http://localhost:9100");
+        deliveryBaseUrl = System.getenv().getOrDefault("AMETO_DELIVERY_URL", "http://localhost:9200/");
         ameto = new Ameto(apiUrl);
     }
 
@@ -81,5 +87,26 @@ public class AmetoTest {
         List<Operator> operators = ameto.getOperators();
 
         assertNotNull(operators);
+    }
+
+    @Test
+    public void testAmetoProcessesJpegImage() throws InterruptedException, IOException {
+        Pipeline pipeline = new Pipeline("jpegTestPipeline", Collections.singletonList(new Pipeline.Step("noop")));
+        ameto.add(pipeline);
+        String assetId = ameto.add(Paths.get("src/test/resources/flower.jpg"));
+        Job job = new Job(assetId, pipeline.getName());
+        ameto.add(job);
+        Thread.sleep(5000L);
+        OkHttpClient http = new OkHttpClient();
+        String assetUrl = deliveryBaseUrl + assetId;
+        Request getProcessedAsset = new Request.Builder()
+                .url(assetUrl)
+                .build();
+
+        Response response = http.newCall(getProcessedAsset).execute();
+
+        byte[] imageBytes = Files.readAllBytes(Paths.get("src/test/resources/flower.jpg"));
+        byte[] processedImageBytes = response.body().bytes();
+        assertArrayEquals(imageBytes, processedImageBytes);
     }
 }
