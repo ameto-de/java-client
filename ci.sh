63227f5ee3d446e6a3437dcb8c0a977f8023ae37
Michael Seifert <m.seifert@digitalernachschub.de>
2018-02-05 13:01:02 +0100

    An API token is set up in the test environment.

--- ci.sh
+++ ci.sh
@@ -9,12 +9,14 @@ setup_test_env() {
     local project_name=$(basename $(pwd))
     local network_name=${project_name//-/}_default
     python3.6 -m ametoctl operators package noop-operator.toml | docker run --interactive --network=${network_name} --rm dev.digitalernachschub.de/ameto/ametoctl:0.4.0 --broker kafka:9092 operators add -
+    local api_token=$(docker run --interactive --network=${network_name} --rm dev.digitalernachschub.de/ameto/ametoctl:0.4.0 --broker kafka:9092 users add_token)
+    export AMETO_API_TOKEN=${api_token}
 }
 
 run_tests() {
     local project_name=$(basename $(pwd))
     local network_name=${project_name//-/}_default
-    docker run --network=${network_name} --env AMETO_API_URL=http://api:5000 \
+    docker run --network=${network_name} --env AMETO_API_URL=http://api:5000 --env AMETO_API_TOKEN=${AMETO_API_TOKEN} \
       --mount type=volume,source=${project_name}_gradle_cache,target=/home/gradle/.gradle --rm ameto/java-client-tests
 }
 
