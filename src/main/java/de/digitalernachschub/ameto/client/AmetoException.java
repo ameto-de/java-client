b02d8a35289c5a83e24bb517ba6ec8dfd0316805
Michael Seifert <m.seifert@digitalernachschub.de>
2018-03-01 09:01:08 +0100

    Added base class for all exceptions thrown by the client library.

---
+++ src/main/java/de/digitalernachschub/ameto/client/AmetoException.java
@@ -0,0 +1,12 @@
+package de.digitalernachschub.ameto.client;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Base class for all exceptions thrown by the Ameto client.
+ */
+public class AmetoException extends RuntimeException {
+    public AmetoException(String message) {
+        super(message);
+    }
+}
