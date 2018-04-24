c6f58278c1f3342ff0d1a451b4e059d1f3e4b6c1
Michael Seifert <m.seifert@digitalernachschub.de>
2018-04-24 09:44:22 +0200

    Updated network name to match network naming of newest docker-compose version.

--- ci.sh
+++ ci.sh
@@ -7,7 +7,7 @@ setup_test_env() {
     docker-compose -f docker-compose.ci.yml up -d
     sleep 10
     local project_name=$(basename $(pwd))
-    local network_name=${project_name//-/}_default
+    local network_name=${project_name}_default
     python3.6 -m ametoctl operators package noop-operator.toml | docker run --interactive --network=${network_name} --rm dev.digitalernachschub.de/ameto/ametoctl:0.5.1 --broker kafka:9092 operators add -
     local api_token=$(docker run --interactive --network=${network_name} --rm dev.digitalernachschub.de/ameto/ametoctl:0.5.1 --broker kafka:9092 users add_token testUser)
     export AMETO_API_TOKEN=${api_token}
@@ -15,7 +15,7 @@ setup_test_env() {
 
 run_tests() {
     local project_name=$(basename $(pwd))
-    local network_name=${project_name//-/}_default
+    local network_name=${project_name}_default
     docker run --network=${network_name} --env AMETO_API_URL=http://api:5000 --env AMETO_API_TOKEN=${AMETO_API_TOKEN} \
       --mount type=volume,source=${project_name}_gradle_cache,target=/home/gradle/.gradle --rm ameto/java-client-tests
 }
