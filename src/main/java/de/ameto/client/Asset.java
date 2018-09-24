0c542c2cc105afb8fe293debba3355c4f370b3bb
Michael Seifert <m.seifert@digitalernachschub.de>
2018-09-24 14:21:10 +0200

    Asset instances are equal if their IDs are equal.

--- src/main/java/de/ameto/client/Asset.java
+++ src/main/java/de/ameto/client/Asset.java
@@ -1,10 +1,12 @@
 package de.ameto.client;
 
+import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 
 @Value
 @RequiredArgsConstructor
+@EqualsAndHashCode(of={"id"})
 public class Asset {
     String id;
 }
