514d3032f81f6c2caa3a156746524bf25e932ae1
Michael Seifert <m.seifert@digitalernachschub.de>
2019-04-04 10:32:22 +0200

    AssetMetadata ignores unknown properties upon deserialization.

--- src/main/java/de/ameto/client/AssetMetadata.java
+++ src/main/java/de/ameto/client/AssetMetadata.java
@@ -1,5 +1,6 @@
 package de.ameto.client;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 
@@ -7,6 +8,7 @@ import java.util.List;
 
 @Value
 @RequiredArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 class AssetMetadata {
     String id;
     List<String> variants;
